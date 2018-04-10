43920f0b03e5ae0e81bc7a4254a5bb25e2d0c5df
EC2 Default User <ec2-user@ip-172-31-42-204.us-west-2.compute.internal>
2018-04-10 14:56:15 +0000

    added figs

--- code/clean.py
+++ code/clean.py
@@ -3,14 +3,23 @@
 # 
 import os.path as path
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg') 
+import missingno as msno
 import sys
 
 # Open the file to be cleaned
 fpath = sys.argv[1]
 if(path.isfile(fpath)):
-    f = pd.read_csv(fpath)
+    f = pd.read_csv(fpath, low_memory=False)
 else:
     print("ERROR: %s is an invalid file path" % fpath)
     exit(1)
 
-print(f.head())
+plt = msno.matrix(f.sample(1000))
+fig = plt.get_figure()
+fig.savefig("missing_projects_data.png")
+
+plt = msno.heatmap(f)
+fig = plt.get_figure()
+fig.savefig("project_collissions.png")
