fb02792c0e8d333d899366db0d60d21a19fa2aab
EC2 Default User <ec2-user@ip-172-31-42-204.us-west-2.compute.internal>
2018-04-15 23:03:10 +0000

    updated partition.py

--- code/partition.py
+++ code/partition.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import sys, os, re
 
-def miniturize(fpath):
+def miniturize(fname):
+    fpath = "data/" + fname
     # Open the file to be cleaned
     if(os.path.isfile(fpath)):
         f = pd.read_csv(fpath, low_memory=False)
     else:
         print("ERROR: %s is an invalid file path" % fpath)
         exit(1)
+    
+    mini = f.iloc[::100, :]
+    mini.to_csv
+    
 
 # Get all of the paths to the data
 paths = [f for f in os.listdir("./data/") if re.match(r".*\.csv", f)]
