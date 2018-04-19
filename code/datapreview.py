b422a5f06f5b67144a0197f95f128ad8b649300d
Andrew Casner <a.casner@comcast.net>
2018-04-18 19:08:36 -0600

    Datapreview works for all small datasets

--- code/datapreview.py
+++ code/datapreview.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
-dependenciesData = pd.read_csv("./data/dependencies-1.2.0-2018-03-12.csv", low_memory=False)
-projectsData = pd.read_csv("./data/projects-1.2.0-2018-03-12.csv", low_memory=False)
-projectsReposData = pd.read_csv("./data/projects_with_repository_fields-1.2.0-2018-03-12.csv", low_memory=False)
-reposData = pd.read_csv("./data/repositories-1.2.0-2018-03-12.csv", low_memory=False)
-reposDependenciesData = pd.read_csv("./data/repository_dependencies-1.2.0-2018-03-12.csv", low_memory=False)
+dependenciesData = pd.read_csv("./small_data/dependencies-1.2.0-2018-03-12.csv", low_memory=False)
+projectsData = pd.read_csv("./small_data/projects-1.2.0-2018-03-12.csv", low_memory=False)
+projectsReposData = pd.read_csv("./small_data/projects_with_repository_fields-1.2.0-2018-03-12.csv", low_memory=False)
+reposData = pd.read_csv("./small_data/repositories-1.2.0-2018-03-12.csv", low_memory=False)
+reposDependenciesData = pd.read_csv("./small_data/repository_dependencies-1.2.0-2018-03-12.csv", low_memory=False)
 
 print(dependenciesData.head(3))
+print(projectsData.head(3))
+print(projectsReposData.head(3))
+print(reposData.head(3))
+print(reposDependenciesData.head(3))
